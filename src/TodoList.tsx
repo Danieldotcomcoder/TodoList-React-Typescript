a3f394fa090af1d21215a33b72588f713527a3e0
danieldotcomcoder <daniel_shdeed@hotmail.com>
2023-10-27 11:33:10 +0300

    install clipboard-copy library

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,47 +1,53 @@
 import React, { useState } from 'react';
 import { Todo } from './type';
+import copy from 'clipboard-copy';
 
 const TodoList: React.FC = () => {
-    const [todos, setTodos] = useState<Todo[]>([]);
-    const [value, setValue] = useState("");
-    const [searchValue, setSearchValue] = useState("");
-  
-    const addTodo: (text: string) => void = (text) => {
-      const newTodos = [...todos, { text, complete: false }];
-      setTodos(newTodos);
-    };
-  
-    const handleSubmit: (event: React.FormEvent) => void = (event) => {
-      event.preventDefault();
-      addTodo(value);
-      setValue("");
-    };
-  
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <button type="submit">Add Todo</button>
-        </form>
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [value, setValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  const addTodo: (text: string) => void = (text) => {
+    const newTodos = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const handleSubmit: (event: React.FormEvent) => void = (event) => {
+    event.preventDefault();
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Search..."
-          onChange={(e) => setSearchValue(e.target.value)}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
-        {todos
-          .filter((todo) => todo.text.includes(searchValue))
-          .map((todo, i) => (
-            <div key={i}>
-              <span style={{ textDecoration: todo.complete ? 'line-through' : undefined }}>
-                {todo.text}
-              </span>
-            </div>
-          ))}
-      </div>
-    );
-  };
-  export default TodoList;
+        <button type="submit">Add Todo</button>
+      </form>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      {todos
+        .filter((todo) => todo.text.includes(searchValue))
+        .map((todo, i) => (
+          <div key={i}>
+            <span
+              style={{
+                textDecoration: todo.complete ? 'line-through' : undefined,
+              }}
+            >
+              {todo.text}
+            </span>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default TodoList;
